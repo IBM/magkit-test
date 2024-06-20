c19f7cdacce92331e969079ef28a701c54ae74a5
Jörg von Frantzius <joerg.frantzius@aperto.com>
2024-06-20 15:08:40 +0200

    #19 turn into JUnit5 extension

--- magkit-test-server/src/main/java/de/ibmix/magkit/test/server/MagnoliaTomcatExtension.java
+++ magkit-test-server/src/main/java/de/ibmix/magkit/test/server/MagnoliaTomcatExtension.java
@@ -9,9 +9,9 @@ package de.ibmix.magkit.test.server;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,15 +20,10 @@ package de.ibmix.magkit.test.server;
  * #L%
  */
 
-import static io.restassured.RestAssured.get;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.apache.catalina.LifecycleException;
@@ -36,51 +31,60 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.groovy.parser.antlr4.util.StringUtils;
 import org.apache.tomcat.util.scan.StandardJarScanner;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import info.magnolia.module.cache.CacheModule;
-import info.magnolia.module.cache.cachepolicy.Default;
-import info.magnolia.objectfactory.Components;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import io.restassured.config.HttpClientConfig;
 import io.restassured.internal.RequestSpecificationImpl;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 /**
- * Starts up Tomcat and executes tests that can both verify requests using
- * RestAssured and inspect Magnolia configuration inside the running Tomcat. 
- * The webapp POM allows to run it using "mvn integration-test -DtomcatTest".
- * Test must be run using special maven-surefire-plugin configuration, please
- * see pom.xml of the example magkit-test-webapp.
- * @author Jörg von Frantzius
+ * Starts up Tomcat with a given Magnolia webapp. This allows to execute tests
+ * that verify requests using RestAssured and at the same time inspect Magnolia
+ * configuration inside the running Tomcat, in order to test e.g. custom
+ * {@link info.magnolia.module.delta.Task}s.
+ * <p>
+ * The example webapp POM in magkit-testwebapp shows how to run it using "mvn
+ * integration-test -DtomcatTest". Test must be run using special
+ * maven-surefire-plugin configuration, please see pom.xml of the example
+ * magkit-test-webapp.
+ * <p>
+ * Expects the WAR file or folder name given as system property
+ * {@link MagnoliaTomcatExtension#WAR_NAME_SYSTEM_PROPERTY}, or as argument to
+ * {@link Builder#setWarName(String)}, and expects that file or folder to exist
+ * in the "target" folder of the current directory.
+ * 
+ * @author joerg.frantzius
  */
-public class TomcatTest {
+public class MagnoliaTomcatExtension implements BeforeAllCallback, AfterAllCallback {
+
+    public static final String WAR_NAME_SYSTEM_PROPERTY = "project.build.finalName";
+    public static final String LOCAL_AUTHOR_URL = "http://127.0.0.1:8080";
+
+    // system property that tells us to set RestAssured request timeouts to
+    // unlimited,
+    // so tests can be debugged
+    public static final String NO_TEST_REQUEST_TIMEOUT_SYSTEM_PROPERTY = "de.ibmix.magkit.test.server.noTestRequestTimeout";
 
     private static final String MAGNOLIA_AUTOUPDATE_CONFIG_PROP = "magnolia.update.auto";
     private static final String MAGNOLIA_JACKRABBIT_CONFIG_PROP = "magnolia.repositories.jackrabbit.config";
     private static final String MAGNOLIA_LOGS_DIR_CONFIG_PROP = "magnolia.logs.dir";
     private static final String MAGNOLIA_REPOSITORIES_HOME_CONFIG_PROP = "magnolia.repositories.home";
 
-    private static final String LOCAL_AUTHOR_URL = "http://127.0.0.1:8080";
-
-    // system property that tells us to set RestAssured request timeouts to unlimited,
-    // so tests can be debugged
-    private static final String NO_TEST_REQUEST_TIMEOUT = "de.ibmix.magkit.test.server.noTestRequestTimeout";
-
-    private static final Tomcat TOMCAT = new Tomcat();
-    private static StandardContext c_webAppContext;
+    private final Tomcat _tomcat = new Tomcat();
+    private StandardContext _webAppContext;
+    private String _warName = System.getProperty(WAR_NAME_SYSTEM_PROPERTY);
 
     /**
-     * Use this instead of RestAssured.given() in order to be able to debug RestAssured tests without timeout,
-     * by setting system property {@link #NO_TEST_REQUEST_TIMEOUT}.
+     * Use this instead of RestAssured.given() in order to be able to debug
+     * RestAssured tests without timeout, by setting system property
+     * {@link #NO_TEST_REQUEST_TIMEOUT_SYSTEM_PROPERTY}.
      */
     public static RequestSpecification given() {
         RequestSpecification given = io.restassured.RestAssured.given();
         // conditionally set request timeout to unlimited to allow for debugging
-        if (Boolean.getBoolean(NO_TEST_REQUEST_TIMEOUT)) {
+        if (Boolean.getBoolean(NO_TEST_REQUEST_TIMEOUT_SYSTEM_PROPERTY)) {
             // if you find a less hacky way of setting the timeout, please apply it,
             // https://stackoverflow.com/a/46913204 simply didn't work for me
             HttpClientConfig noTimeoutConfig = HttpClientConfig.httpClientConfig().setParam("http.socket.timeout", 0);
@@ -94,7 +98,7 @@ public class TomcatTest {
      *
      * @throws IOException
      */
-    private static void configureMagnolia() throws IOException {
+    private void configureMagnolia() throws IOException {
         Path currentDir = Paths.get("").toAbsolutePath();
         Path magnoliaDir = currentDir.resolve("target/magnolia");
 
@@ -115,11 +119,11 @@ public class TomcatTest {
         System.setProperty(MAGNOLIA_AUTOUPDATE_CONFIG_PROP, "true");
     }
 
-    @BeforeAll
-    public static void startTomcat() throws LifecycleException, IOException {
-        String warName = System.getProperty("project.build.finalName");
-        if (StringUtils.isEmpty(warName)) {
-            throw new RuntimeException("TomcatTest expectation failed: could not find system property project.build.finalName, please configure maven-surefire-plugin to pass this as a system property as shown here: https://maven.apache.org/surefire/maven-surefire-plugin/examples/system-properties.html");
+    @Override
+    public void beforeAll(ExtensionContext context) throws IOException, LifecycleException {
+        if (StringUtils.isEmpty(_warName)) {
+            throw new RuntimeException(
+                    "TomcatTest expectation failed: given warName is empty, and system property project.build.finalName is also empty, please configure maven-surefire-plugin to pass this as a system property as shown here: https://maven.apache.org/surefire/maven-surefire-plugin/examples/system-properties.html");
         }
         configureMagnolia();
 
@@ -128,74 +132,79 @@ public class TomcatTest {
         Path tomcatDir = currentDir.resolve("target/TomcatTest");
         Files.createDirectories(tomcatDir);
 
-        TOMCAT.setPort(8080);
-        TOMCAT.setBaseDir("./target/TomcatTest");
-        TOMCAT.getHost().setAutoDeploy(true);
-        Path webAppDir = currentDir.resolve("target/" + warName);
-        c_webAppContext = (StandardContext) TOMCAT.addWebapp("", webAppDir.toFile().getAbsolutePath());
+        _tomcat.setPort(8080);
+        _tomcat.setBaseDir("./target/TomcatTest");
+        _tomcat.getHost().setAutoDeploy(true);
+        Path webAppDir = currentDir.resolve("target/" + _warName);
+        _webAppContext = (StandardContext) _tomcat.addWebapp("", webAppDir.toFile().getAbsolutePath());
         // prevent Tomcat from scanning for web fragments in the JVM classpath (i.e. not
         // in the web app classpath),
         // as StandardJarScanner.processManifest() otherwise produces bogus classpath
         // entries for non-existant
         // JARs found in the manifest of vaadin-server
-        ((StandardJarScanner) c_webAppContext.getJarScanner()).setScanClassPath(false);
+        ((StandardJarScanner) _webAppContext.getJarScanner()).setScanClassPath(false);
         // make Tomcat use the Surefire classloader, or we get ClassCastExceptions
         // because Magnolia classes
         // loaded by our test use the Surefire classloader, while Magnolia in the webapp
         // uses the
         // Tomcat webapp classloader
-        c_webAppContext.setParentClassLoader(TomcatTest.class.getClassLoader());
-        c_webAppContext.setDelegate(true);
+        _webAppContext.setParentClassLoader(MagnoliaTomcatExtension.class.getClassLoader());
+        _webAppContext.setDelegate(true);
 
-        TOMCAT.init();
+        _tomcat.init();
         // must call getConnector(), see https://stackoverflow.com/a/59282431/1245428
-        TOMCAT.getConnector();
-        TOMCAT.start();
+        _tomcat.getConnector();
+        _tomcat.start();
 
         // run server loop that waits for shutdown command
         CompletableFuture.supplyAsync(() -> {
-            TOMCAT.getServer().await();
+            _tomcat.getServer().await();
             return true;
         });
     }
 
-    @AfterAll
-    public static void shutdownTomcat() throws LifecycleException {
-        TOMCAT.stop();
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        _tomcat.stop();
+    }
+
+    public String getServerUrl() {
+        return LOCAL_AUTHOR_URL;
     }
 
     /**
-     * Example of a test using RestAssured against Magnolia running in Tomcat:
-     * verify that local Magnolia superuser can login.
+     * Provides Builder pattern as in
+     * https://github.com/junit-team/junit5/blob/main/documentation/src/main/java/example/registration/WebServerExtension.java
+     * .
      */
-    @Test
-    public void testLocalSuperuserLogin() throws Exception {
-
-        Response response = get(LOCAL_AUTHOR_URL + "/.magnolia/admincentral");
-        assertEquals(401, response.getStatusCode());
-        String csrfCookie = response.getCookie("csrf");
-        assertNotNull(csrfCookie);
-
-        loginLocalSuperuser(LOCAL_AUTHOR_URL, csrfCookie).then().statusCode(302);
-
+    public static Builder builder() {
+        return new Builder();
     }
 
-    private Response loginLocalSuperuser(String address, String csrfCookie) {
-        return given().cookie("csrf", csrfCookie)
-                .formParams(Map.of("mgnlUserId", "superuser", "mgnlUserPSWD", "superuser", "csrf", csrfCookie)).when()
-                .post(address + "/.magnolia/admincentral");
-    }
+    public static class Builder {
+        private String _warName;
 
-    /**
-     * Example of a test that accesses the running Magnolia's object registry to verify some configuration
-     * is as expected, e.g. as written by some UpdateTask
-     */
-    @Test
-    public void testMagnoliaConfig() {
-        CacheModule cacheModule = Components.getComponent(CacheModule.class);
-        Default cachePolicy = (Default) cacheModule.getContentCaching("defaultPageCache").getCachePolicy();
-        assertNotNull(cachePolicy, "Expected defaultPageCache config to exist!");
-    }
+        /**
+         * Set the name of the WAR file / folder to be deployed, which is expected to
+         * exist within the "target" folder of the current directory. If it is not set
+         * here, {@link MagnoliaTomcatExtension} expects a system propery
+         */
+        public Builder setWarName(String warNameArg) {
+            if (StringUtils.isEmpty(warNameArg)) {
+                throw new IllegalArgumentException("warName must not be empty");
+            }
+            _warName = warNameArg;
+            return this;
+        }
 
+        public MagnoliaTomcatExtension build() {
+            MagnoliaTomcatExtension tomcatExtension = new MagnoliaTomcatExtension();
+            if (_warName != null) {
+                tomcatExtension._warName = _warName;
+            }
+            return tomcatExtension;
+        }
+
+    }
 
 }
